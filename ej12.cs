bd5f0b23eeaf4c9e499533b4f0bb0e221fb233a8
Adrián Mora Rodríguez <moradrian03@gmail.com>
2024-10-14 18:18:29 +0100

    Arreglados los FixedUpdate

--- ej12.cs
+++ ej12.cs
@@ -11,7 +11,7 @@ public class ej12 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
       float moveH = Input.GetAxis("Horizontal");
       float moveV = Input.GetAxis("Vertical");
       if (moveH != 0 || moveV != 0) {
--- mov_ej_10.cs
+++ mov_ej_10.cs
@@ -15,16 +15,18 @@ public class mov_ej_10 : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-      float h = Input.GetAxis("Horizontal");
-      float v = Input.GetAxis("Vertical");
-			if (h != 0 || v != 0) {
-      	cube.GetComponent<Transform>().Translate(h * speed * Time.deltaTime, 0, v * speed * Time.deltaTime);
-			}
       float moveH = Input.GetAxis("HorizontalSphere"); 
       float moveV = Input.GetAxis("VerticalSphere");
 			if (moveH != 0 || moveV != 0) {
         Vector3 movement = new Vector3(moveH * speedSphere * Time.deltaTime, 0, moveV * speedSphere * Time.deltaTime);
         sphere.GetComponent<Rigidbody>().AddForce(movement);
+			}
+    }
+    void FixedUpdate() {
+      float h = Input.GetAxis("Horizontal");
+      float v = Input.GetAxis("Vertical");
+			if (h != 0 || v != 0) {
+      	cube.GetComponent<Transform>().Translate(h * speed * Time.deltaTime, 0, v * speed * Time.deltaTime);
 			}
     }
 }
